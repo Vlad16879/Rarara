21415417a266131400d75598bd4df510a6f12343
Vlad16879 <xxxflayqpayxxx@gmail.com>
2025-08-09 23:45:22 +0300

    Update esp.lua

--- esp.lua
+++ esp.lua
@@ -5,8 +5,10 @@ while task.wait(0.2) do
     for _, item in pairs(workspace:GetChildren()) do
         if item:IsA("Part") or item:IsA("MeshPart") then
             if item.Name == "Fruit" or item.Name == "Seed" or item.Name == "Flower" then
+                hrp.CFrame = item.CFrame
                 firetouchinterest(hrp, item, 0)
                 firetouchinterest(hrp, item, 1)
+                task.wait(0.1)
             end
         end
     end
